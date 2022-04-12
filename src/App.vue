71ac0b929cde41db89e771cb22d0c0d0b906c861
Cheng_DX <2337040896@qq.com>
2022-04-12 21:17:41 +0800

    feat: init

---
+++ src/App.vue
@@ -0,0 +1,189 @@
+<script setup lang="ts">
+import { computed } from '@vue/reactivity'
+import { nextTick, ref, watch } from 'vue'
+import ColorPicker from './ColorPicker.vue'
+
+const modes = [
+  {
+    name: 'large',
+    size: 30
+  },
+  {
+    name: 'medium',
+    size: 20
+  },
+  {
+    name: 'small',
+    size: 10
+  }
+]
+const mode = ref('medium')
+const size = computed(() => {
+  const currentMode = modes.find(m => m.name === mode.value)
+  return currentMode?.size ? currentMode.size : 0
+})
+watch(mode, () => {
+  pixels.value = Array.from({ length: size.value }, () =>
+    Array.from({ length: size.value }, () => '')
+  )
+})
+function switchMode() {
+  switch (mode.value) {
+    case 'large':
+      mode.value = 'medium'
+      break
+    case 'medium':
+      mode.value = 'small'
+      break
+    case 'small':
+      mode.value = 'large'
+      break
+    default:
+      mode.value = 'large'
+  }
+}
+const color = ref('')
+
+const pixels = ref(
+  Array.from({ length: size.value }, () =>
+    Array.from({ length: size.value }, () => '')
+  )
+)
+function clickBox(idx1: number, idx2: number) {
+  pixels.value[idx1][idx2] = color.value
+}
+function exportJSON() {
+  const result = []
+  for (let i = 0; i < size.value; i++) {
+    for (let j = 0; j < size.value; j++) {
+      if (pixels.value[i][j]) {
+        result.push({
+          x: i,
+          y: j,
+          color: pixels.value[i][j]
+        })
+      }
+    }
+  }
+  const json = {
+    mode: mode.value,
+    data: result
+  }
+  const a = document.createElement('a')
+  a.href =
+    'data:text/json;charset=utf-8,' + encodeURIComponent(JSON.stringify(json))
+  a.download = 'pixels.json'
+  a.click()
+}
+async function handleFile(event: any) {
+  const file = event.target.files[0]
+  const { mode: savedMode, data } = JSON.parse(await file.text())
+  if (data.length === 0) {
+    alert('EMPTY FILE')
+    return
+  }
+  mode.value = savedMode
+  _clear()
+  nextTick(() => {
+    for (let item of data) {
+      pixels.value[item.x][item.y] = item.color
+    }
+  })
+}
+
+function _clear() {
+  pixels.value = Array.from({ length: size.value }, () =>
+    Array.from({ length: size.value }, () => '')
+  )
+}
+</script>
+
+<template>
+  <div class="root">
+    <div class="tools">
+      <button @click="switchMode" class="export-button">
+        {{ mode.toUpperCase() }}
+      </button>
+      <color-picker v-model="color" />
+      <div class="button-group">
+        <label for="uploadFile" class="label">
+          <button class="export-button">IMPORT</button>
+          <input
+            type="file"
+            @change="handleFile"
+            name="upload"
+            accept=".json"
+            class="upload-file"
+          />
+        </label>
+        <button @click="exportJSON" class="export-button">EXPORT</button>
+      </div>
+    </div>
+
+    <div v-for="(row, idx1) in pixels" class="row">
+      <div
+        @click="clickBox(idx1, idx2)"
+        v-for="(item, idx2) in row"
+        class="pixel"
+        :style="{
+          backgroundColor: item
+        }"
+        :key="`${idx1}-${idx2}`"
+      ></div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.root {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+.tools {
+  display: flex;
+  width: 670px;
+  height: 100px;
+  flex-direction: row;
+  justify-content: space-around;
+  align-items: center;
+}
+.button-group {
+  display: flex;
+  height: 100%;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-evenly;
+}
+.export-button {
+  height: 37px;
+  width: 100px;
+  border: 1px solid #ffffff1f;
+  border-radius: 5px;
+  background-color: #ffffff1f;
+  color: #ffffff;
+  cursor: pointer;
+  font-weight: 700;
+}
+.label {
+  position: relative;
+}
+.upload-file {
+  position: absolute;
+  left: 0;
+  top: 0;
+  opacity: 0;
+  cursor: pointer;
+  height: 37px;
+  width: 100px;
+}
+.row {
+  display: flex;
+  flex-direction: row;
+}
+.pixel {
+  width: v-bind(600 / size + 'px');
+  height: v-bind(600 / size + 'px');
+  border: 1px dotted #ffffff1f;
+}
+</style>
---
+++ src/ColorPicker.vue
@@ -0,0 +1,60 @@
+<script setup lang="ts">
+defineProps<{
+  modelValue: string
+}>()
+defineEmits(['update:modelValue'])
+const colors = [
+  '#006778',
+  '#0093AB',
+  '#00AFC1',
+  '#FFD124',
+  '#F90716',
+  '#FF5403',
+  '#FFCA03',
+  '#FFF323'
+]
+</script>
+
+<template>
+  <div class="color-picker-root">
+    <div class="box"></div>
+    <div class="select-panel">
+      <div
+        v-for="color in colors"
+        @click="$emit('update:modelValue', color)"
+        :style="{
+          backgroundColor: color
+        }"
+        class="color-box"
+      ></div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.color-picker-root {
+  display: flex;
+  flex-direction: row;
+  height: 100px;
+}
+.box {
+  height: 70px;
+  width: 70px;
+  background-color: v-bind(modelValue);
+  border: 1px solid #ffffff1f;
+  margin-block: auto;
+}
+.select-panel {
+  display: flex;
+  flex-wrap: wrap;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  margin-left: 5px;
+}
+.color-box {
+  height: 30px;
+  width: 30px;
+  margin: 3px;
+}
+</style>
