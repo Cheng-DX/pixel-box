a02813e7764d92c8d4dc4aa713de11421306e842
Cheng_DX <2337040896@qq.com>
2022-04-12 21:25:51 +0800

    chore: update configs

--- vite.config.ts
+++ vite.config.ts
@@ -5,10 +5,14 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/pixel-box/',
   plugins: [vue()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
+  },
+  build: {
+    outDir: 'docs'
   }
 })
